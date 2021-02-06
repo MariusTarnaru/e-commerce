d21be4342c6115387b83327d5137be31b2839a63
Marius.TARNARU <mtarnaru@yahoo.com>
2021-02-06 16:00:39 +0200

    1 st commit - Angular my shoping website - see 100 products from DB in 4 categories dynamically (with routerLink)

---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import {ProductService} from '../../services/product.service';
+import {Product} from '../../common/product';
+import {ActivatedRoute} from '@angular/router';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list-grid.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  products: Product[];
+  currentCategoryId: number;
+
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe( () => {
+      this.listProducts();
+    });
+  }
+
+  listProducts(): void {
+    // check if "id" parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      // get the "id" param string and convert string to number using the "+" symbol
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+    }
+    else {
+      // not category id available.... default to category id 1
+      this.currentCategoryId = 1;
+    }
+    // now get the products for the given category id
+    this.productService.getProductList(this.currentCategoryId).subscribe(
+      data => {
+        this.products = data;
+      }
+    );
+  }
+}
